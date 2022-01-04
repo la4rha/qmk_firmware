c5ef8ae8e8dee98fef3a8e86179f07dcbe966c82
LarsF <la4rha@gmail.com>
2022-01-04 08:56:32 +0100

    Static linking

--- keyboards/crkbd/keymaps/la4rha/rules.mk
+++ keyboards/crkbd/keymaps/la4rha/rules.mk
@@ -10,6 +10,9 @@ MOUSEKEY_ENABLE = yes # Mouse keys
 EXTRAKEY_ENABLE = yes # Audio control and System control
 AUTO_SHIFT_ENABLE = yes # Auto Shift
 
+# Static linking			https://docs.qmk.fm/#/squeezing_avr
+LTO_ENABLE = yes
+
 #SRC += manna-harbour_miryoku.c # keymap
 
 # alternative layouts:
